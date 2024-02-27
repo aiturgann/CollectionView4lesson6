a16ba674292fd446a819719e2160c2a4d08e0326
aiturann <aiturgannnnnn@gmail.com>
2024-02-27 20:01:58 +0600

    Initial Commit

---
+++ CollectionView4lesson6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView4lesson6
+//
+//  Created by Aiturgan Kurmanbekova on 27/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
